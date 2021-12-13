fb6528a8018ce5d23a89d8829845f093f13b3f80
datejer <ejer.priv@wp.pl>
2021-12-13 11:51:20 +0100

    Add Require Podcast banner to /stuff

--- components/StuffPage/StuffPage.js
+++ components/StuffPage/StuffPage.js
@@ -4,10 +4,27 @@ import Technologies from '../Technologies/Technologies';
 
 import styles from './StuffPage.module.scss';
 
+import Require from '../../assets/icons/require.svg';
+
 export default function StuffPage() {
 	return (
 		<div>
 			<SEO title="stuff" description="stuff i made" />
+			<a href="https://require.podcast.gq">
+				<div className={styles.project} style={{ borderColor: '#ff5370' }}>
+					<div>
+						<h2 className={styles.projectName}>Require Podcast</h2>
+						<p className={styles.projectDesc}>
+							Polski podcast na temat JavaScriptu i nie tylko. "Na luzie" o
+							technologiach frontendowych, przez nowości ze świata IT, po
+							frameworki na backendzie!
+						</p>
+					</div>
+					<div>
+						<Require />
+					</div>
+				</div>
+			</a>
 			<div>
 				<h1 className={styles.title}>stuff i made.</h1>
 				<PinnedRepos />
