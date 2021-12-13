34eda484638afe31349a82f2175dd1462b63bedc
datejer <ejer.priv@wp.pl>
2021-12-13 18:07:57 +0100

    Update LinkedIn url

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
 		return [
 			{
 				source: '/linkedin',
-				destination: 'https://www.linkedin.com/in/artur-dudek-5684741ba/',
+				destination: 'https://www.linkedin.com/in/artur-dudek/',
 				permanent: true,
 			},
 		];
