c7e340d744c6f8c71344403c928241949f57b839
datejer <ejer.priv@wp.pl>
2021-12-14 22:30:58 +0100

    Add Skeletons and Placeholders for PinnedRepos

--- components/PinnedRepos/PinnedRepos.js
+++ components/PinnedRepos/PinnedRepos.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import dynamic from 'next/dynamic';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+import { useTheme } from 'next-themes';
 
 const Tilty = dynamic(() => import('react-tilty'), { ssr: false });
 
@@ -7,6 +9,8 @@ import styles from './PinnedRepos.module.scss';
 
 export default function PinnedRepos() {
 	const [pinned, setPinned] = useState(new Array(6).fill({}));
+	const { theme } = useTheme();
+	const [currentTheme, setCurrentTheme] = useState('dark');
 
 	useEffect(() => {
 		fetch('/api/pinned?username=datejer')
@@ -14,29 +18,58 @@ export default function PinnedRepos() {
 			.then(data => setPinned(data));
 	}, []);
 
+	useEffect(() => {
+		setCurrentTheme(theme);
+	}, [theme]);
+
 	return (
 		<div className={styles.pinned}>
-			{pinned.map((repo, index) => (
-				<div key={repo.repo || index} className={styles.repoWrapper}>
-					<Tilty
-						style={{ height: '100%', borderRadius: '0.75rem' }}
-						gyroscope={false}
-					>
-						<a href={repo.link}>
-							<div className={styles.repo} style={{ borderColor: repo.color }}>
-								<h2 className={styles.repoName}>
-									{repo.owner}/{repo.repo}
-								</h2>
-								<p className={styles.repoDescription}>{repo.description}</p>
-								<div className={styles.repoStats}>
-									<div>⭐ {repo.stars}</div>
-									<div>🍴 {repo.forks}</div>
+			<SkeletonTheme
+				baseColor={currentTheme === 'dark' ? '#202020' : ''}
+				highlightColor={currentTheme === 'dark' ? '#444' : ''}
+			>
+				{pinned.map((repo, index) => (
+					<div key={repo.repo || index} className={styles.repoWrapper}>
+						<Tilty
+							style={{ height: '100%', borderRadius: '0.75rem' }}
+							gyroscope={false}
+						>
+							<a href={repo.link}>
+								<div
+									className={styles.repo}
+									style={{ borderColor: repo.color }}
+								>
+									<h2 className={styles.repoName}>
+										{repo.repo ? `${repo.owner}/${repo.repo}` : <Skeleton />}
+									</h2>
+
+									<p className={styles.repoDescription}>
+										{repo.description || <Skeleton count={2} />}
+									</p>
+									<div className={styles.repoStats}>
+										<div>
+											⭐{' '}
+											{repo.stars || (
+												<span style={{ fontFamily: 'NorumIpnum' }}>
+													{Math.floor(Math.random() * 99)}
+												</span>
+											)}
+										</div>
+										<div>
+											🍴{' '}
+											{repo.forks || (
+												<span style={{ fontFamily: 'NorumIpnum' }}>
+													{Math.floor(Math.random() * 99)}
+												</span>
+											)}
+										</div>
+									</div>
 								</div>
-							</div>
-						</a>
-					</Tilty>
-				</div>
-			))}
+							</a>
+						</Tilty>
+					</div>
+				))}
+			</SkeletonTheme>
 		</div>
 	);
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { ThemeProvider } from 'next-themes';
 import '../styles/globals.css';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 function MyApp({ Component, pageProps }) {
 	return (
