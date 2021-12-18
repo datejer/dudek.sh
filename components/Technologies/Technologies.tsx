f26fc1501f96d24961b0048bdf2565604dc7eb35
ejer <ejer.priv@wp.pl>
2021-12-18 13:57:57 +0100

    :sparkles: Add more technologies
    TypeScript, Styled-Components, Git, Vercel

--- components/Technologies/Technologies.tsx
+++ components/Technologies/Technologies.tsx
@@ -3,6 +3,7 @@ import clsx from "clsx";
 import styles from "./Technologies.module.scss";
 
 import JavaScript from "../../assets/icons/javascript.svg";
+import TypeScript from "../../assets/icons/typescript.svg";
 import Node from "../../assets/icons/nodedotjs.svg";
 import React from "../../assets/icons/react.svg";
 import Next from "../../assets/icons/nextdotjs.svg";
@@ -10,6 +11,9 @@ import Gatsby from "../../assets/icons/gatsby.svg";
 import Sass from "../../assets/icons/sass.svg";
 import MongoDB from "../../assets/icons/mongodb.svg";
 import Discord from "../../assets/icons/discord.svg";
+import StyledComponents from "../../assets/icons/styledcomponents.svg";
+import Git from "../../assets/icons/git.svg";
+import Vercel from "../../assets/icons/vercel.svg";
 
 export function Technologies() {
   return (
@@ -21,6 +25,12 @@ export function Technologies() {
         <span data-visually-hidden>JavaScript</span>
         <span aria-hidden="true">
           <JavaScript />
+        </span>
+      </a>
+      <a href="https://www.typescriptlang.org/" className={clsx(styles.tech, styles.ts)}>
+        <span data-visually-hidden>TypeScript</span>
+        <span aria-hidden="true">
+          <TypeScript />
         </span>
       </a>
       <a href="https://nodejs.org/" className={clsx(styles.tech, styles.node)}>
@@ -53,6 +63,12 @@ export function Technologies() {
           <Sass />
         </span>
       </a>
+      <a href="https://styled-components.com/" className={clsx(styles.tech, styles.sc)}>
+        <span data-visually-hidden>Styled Components</span>
+        <span aria-hidden="true">
+          <StyledComponents />
+        </span>
+      </a>
       <a href="https://www.mongodb.com/" className={clsx(styles.tech, styles.mongo)}>
         <span data-visually-hidden>MongoDB</span>
         <span aria-hidden="true">
@@ -63,6 +79,18 @@ export function Technologies() {
         <span data-visually-hidden>Discord.js</span>
         <span aria-hidden="true">
           <Discord />
+        </span>
+      </a>
+      <a href="https://git-scm.com/" className={clsx(styles.tech, styles.git)}>
+        <span data-visually-hidden>Git Version Control</span>
+        <span aria-hidden="true">
+          <Git />
+        </span>
+      </a>
+      <a href="https://vercel.com/" className={clsx(styles.tech, styles.vercel)}>
+        <span data-visually-hidden>Vercel</span>
+        <span aria-hidden="true">
+          <Vercel />
         </span>
       </a>
     </div>
