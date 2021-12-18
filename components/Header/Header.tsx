1022edb15aeaef6105d4df9983d90ea5d98f10b9
ejer <ejer.priv@wp.pl>
2021-12-18 17:20:18 +0100

    :sparkles: Make navbar sticky with blurred background

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -15,45 +15,47 @@ export function Header() {
   };
 
   return (
-    <nav className={styles.container}>
-      <ul className={styles.list}>
-        <li>
-          <Link href="/">
-            <a className={styles.button}>
-              <span data-visually-hidden>Main page</span>
-              <span aria-hidden="true">/</span>
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/stuff">
-            <a className={styles.button}>/stuff</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/uses">
-            <a className={styles.button}>/uses</a>
-          </Link>
-        </li>
-      </ul>
-      <ul className={styles.list}>
-        <li>
-          {mounted ? (
-            <button
-              aria-pressed={theme === "dark"}
-              className={styles.themeSwitch}
-              onClick={handleThemeChange}
-            >
-              <span aria-hidden="true">{theme === "dark" ? "🌞" : "🌙"}</span>
-              <span data-visually-hidden>
-                {theme === "dark" ? "Dark theme enabled" : "White theme enabled"}
-              </span>
-            </button>
-          ) : (
-            <button className={styles.themeSwitch}>🌞</button>
-          )}
-        </li>
-      </ul>
-    </nav>
+    <div className={styles.wrapper}>
+      <nav className={styles.container}>
+        <ul className={styles.list}>
+          <li>
+            <Link href="/">
+              <a className={styles.button}>
+                <span data-visually-hidden>Main page</span>
+                <span aria-hidden="true">/</span>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/stuff">
+              <a className={styles.button}>/stuff</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/uses">
+              <a className={styles.button}>/uses</a>
+            </Link>
+          </li>
+        </ul>
+        <ul className={styles.list}>
+          <li>
+            {mounted ? (
+              <button
+                aria-pressed={theme === "dark"}
+                className={styles.themeSwitch}
+                onClick={handleThemeChange}
+              >
+                <span aria-hidden="true">{theme === "dark" ? "🌞" : "🌙"}</span>
+                <span data-visually-hidden>
+                  {theme === "dark" ? "Dark theme enabled" : "White theme enabled"}
+                </span>
+              </button>
+            ) : (
+              <button className={styles.themeSwitch}>🌞</button>
+            )}
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
