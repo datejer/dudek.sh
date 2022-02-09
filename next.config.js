d9a0838f5e645f82ae761f52a11bc09502470590
ejer <ejer.priv@wp.pl>
2022-02-09 21:35:34 +0100

    :lock: Fix CORS for api routes.

--- next.config.js
+++ next.config.js
@@ -18,6 +18,23 @@ module.exports = {
       },
     ];
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     domains: ["cdn.discordapp.com", "i.scdn.co", "media.discordapp.net"],
   },
