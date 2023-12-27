f2584b887acde5fd3ab088253ba3e0e33d40bda2
ejer <ejer.priv@wp.pl>
2023-12-27 18:21:04 +0100

    run codemod

--- components/boards/BoardInfoPage/BoardInfoPage.tsx
+++ components/boards/BoardInfoPage/BoardInfoPage.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 import Link from "next/link";
 import { SEO } from "../../common/SEO";
 import type { Board } from "../../../pages/boards/[name]";
